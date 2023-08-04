9c4cfdf2db3e609d18ff53f79b87cb18ce7421b0
Pedro Henrique <ph.laurindo2017@gmail.com>
2023-08-04 08:27:54 -0300

    Frist class: Position

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position pos = new Position(2, 3);
+		System.out.println(pos);
 
 	}
 
